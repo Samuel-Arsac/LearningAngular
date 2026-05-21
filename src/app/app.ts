8c20d8b058d0e9379b806e80bddb29244725724e
Samuel ARSAC <samuel.arsac@etu.u-bordeaux.fr>
2026-05-21 11:43:49 +0200

    add new component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FaceSnap } from './face-snap/face-snap'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FaceSnap],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
